16920abf3f14e063712f4752613e82cfea1a338b
Limber <it@limberenergy.com>
2026-01-06 17:29:05 +0530

    create class based custom error handler

---
+++ TheRightScoop/TheRightScoop/api _response/custom_error_handler.py
@@ -0,0 +1,14 @@
+from rest_framework.views import exception_handler
+from .custom_response_handler import APIResponse
+
+
+def custom_exception_hanlder(exc,context):
+    response=exception_handler(exc,context)
+
+    if response is not None:
+        return APIResponse.error(
+            message="something went wrong",
+            errors=response.data,
+            status_code=response.status_code
+        )
+    return response
